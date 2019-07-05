7536e674306eb9cb55d005ffc28899287446af78
8020 <cjr8020@gmail.com>
2019-07-05 14:13:55 -0700

    more operations

--- src/main/java/com/demo/rest/ActorRestController.java
+++ src/main/java/com/demo/rest/ActorRestController.java
@@ -6,10 +6,10 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,7 +37,6 @@ public class ActorRestController {
       actorService.saveActor(actor);
       responseEntity = new ResponseEntity<>(
           actor,
-          new HttpHeaders(),
           HttpStatus.CREATED);
 
     } finally {
@@ -49,6 +48,69 @@ public class ActorRestController {
 
     return responseEntity;
   }
+
+  @RequestMapping(
+      value = "/actor",
+      method = RequestMethod.PUT,
+      consumes = "application/json; charset=UTF-8"
+  )
+  public ResponseEntity<Object> updateActor(@Valid @RequestBody final Actor actor) {
+    final StopWatch sw = new StopWatch();
+    ResponseEntity<Object> responseEntity;
+
+    try {
+      sw.start();
+      actorService.saveActor(actor);
+      responseEntity = new ResponseEntity<>(
+          actor,
+          HttpStatus.OK);
+
+    } finally {
+      sw.stop();
+      if (logger.isInfoEnabled()) {
+        logger.info("updateActor completed in {}ms", sw.getTotalTimeMillis());
+      }
+    }
+
+    return responseEntity;
+  }
+
+
+  /**
+   * Fetches actor by id.
+   *
+   * @return ResponseEntity
+   */
+  @RequestMapping(
+      value = "/actor/{id}",
+      method = RequestMethod.GET)
+  public ResponseEntity<Object> getActor(@PathVariable Integer id) {
+
+    ResponseEntity<Object> responseEntity;
+    StopWatch stopWatch = new StopWatch();
+
+    if (logger.isInfoEnabled()) {
+      logger.info("processing 'getActor' request");
+    }
+
+    try {
+      stopWatch.start();
+      Actor actor = actorService.findById(id);
+      responseEntity = new ResponseEntity<>(
+          actor,
+          HttpStatus.OK);
+    } finally {
+      stopWatch.stop();
+      if (logger.isInfoEnabled()) {
+        logger.info("'getActor({})` completed in {}ms",
+            id,
+            stopWatch.getTotalTimeMillis());
+      }
+    }
+
+    return responseEntity;
+  }
+
 
   /**
    * Provides a list of actors.
--- src/main/java/com/demo/service/ActorServiceImpl.java
+++ src/main/java/com/demo/service/ActorServiceImpl.java
@@ -5,11 +5,13 @@ import com.demo.repository.ActorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
+@CacheConfig(cacheNames = "actors-cache")
 public class ActorServiceImpl implements ActorService {
 
   private static final Logger logger = LoggerFactory.getLogger(ActorServiceImpl.class);
@@ -21,21 +23,38 @@ public class ActorServiceImpl implements ActorService {
     this.actorRepository = actorRepository;
   }
 
+  @Cacheable
   public Actor findById(Integer id) {
+    if (logger.isInfoEnabled()) {
+      logger.info("method `findById()` -> fetching data from DB");
+    }
     return actorRepository.findById(id);
   }
 
-  @CacheEvict(value = "actors", allEntries = true)
+  /**
+   * Creates or updates an Actor record.
+   * This method will also clear `allEntries` to ensure removal of stale data.
+   * @param actor object contains new data
+   */
+  @CacheEvict(allEntries = true)
   public void saveActor(final Actor actor) {
+    if (logger.isInfoEnabled()) {
+      logger.info("executing `saveActor()`");
+    }
     actorRepository.save(actor);
   }
 
-  @Cacheable({"actors"})
+  @Cacheable(key = "#root.methodName")
   public Iterable<Actor> readActors() {
     if (logger.isInfoEnabled()) {
       logger.info("method `readActors()` -> going to the DB to get DATA!");
     }
     return actorRepository.findAll();
+  }
+
+  @CacheEvict(allEntries = true)
+  public void clearActorsCache() {
+    // Intentionally blank
   }
 
 }
