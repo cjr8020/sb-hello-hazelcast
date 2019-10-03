1cdb8826a14c7dc0acb29aa0586dbe756f953c6d
8020 <cjr8020@gmail.com>
2019-10-03 14:31:29 -0700

    update hazelcast config to standalone

--- src/main/java/com/demo/configuration/CacheConfiguration.java
+++ src/main/java/com/demo/configuration/CacheConfiguration.java
@@ -4,8 +4,12 @@ import com.demo.domain.Actor;
 import com.demo.domain.serialization.ActorKryoSerializer;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.InterfacesConfig;
+import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MaxSizeConfig;
+import com.hazelcast.config.MaxSizeConfig.MaxSizePolicy;
+import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.config.SerializerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,22 +20,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CacheConfiguration {
 
+  /**
+   * Hazelcast map configuration.
+   * @return MapConfig
+   */
   @Bean
-  public MapConfig mapConfig() {
-    return new MapConfig("default")
-        .setMaxSizeConfig(new MaxSizeConfig(300, MaxSizeConfig.MaxSizePolicy.PER_NODE))
-        .setEvictionPolicy(EvictionPolicy.LRU)
-        .setTimeToLiveSeconds(20); // 20 sec
+  public MapConfig actorsMapConfig() {
+    return new MapConfig("actors-cache")
+        .setMaxSizeConfig(new MaxSizeConfig(100, MaxSizePolicy.PER_NODE))
+        .setEvictionPolicy(EvictionPolicy.LFU)
+        .setTimeToLiveSeconds(3600); // auth token lifespan is 1 hour
   }
 
+  /**
+   * Hazelcast config.
+   * @return Config
+   */
   @Bean
-  public Config hazelcastConfig(MapConfig mapConfig) {
-    Config hazelcastConfig =  new Config("demo-hazelcast-instance");
+  public Config hazelcastConfig() {
+    Config hazelcastConfig = new Config("hello  -hazelcast-instance");
     hazelcastConfig.setProperty("hazelcast.phone.home.enabled", "false");
-    hazelcastConfig.addMapConfig(mapConfig);  // 20 seconds
-    // actor serializer
-    hazelcastConfig.getSerializationConfig().addSerializerConfig(actorSerializerConfig());
+    hazelcastConfig.addMapConfig(actorsMapConfig());  // tokens map
 
+    // network config
+
+    // disable bind to any interface
+    hazelcastConfig.setProperty("hazelcast.socket.bind.any", "false");
+    NetworkConfig networkConfig = hazelcastConfig.getNetworkConfig();
+    InterfacesConfig interfacesConfig = networkConfig.getInterfaces();
+    interfacesConfig.setEnabled(true)
+        .addInterface("127.0.0.1");
+
+    // join config
+    JoinConfig joinConfig = networkConfig.getJoin();
+    joinConfig.getMulticastConfig().setEnabled(false);
+    joinConfig.getTcpIpConfig().setEnabled(false);
     return hazelcastConfig;
   }
 
